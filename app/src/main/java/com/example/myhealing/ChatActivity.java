71b1e1130a071d55ce69fc43ae5b10f1378099be
Woo_jin <waw1159@gmail.com>
2023-10-16 23:08:50 +0900

    add create_diary button

--- app/src/main/java/com/example/myhealing/ChatActivity.java
+++ app/src/main/java/com/example/myhealing/ChatActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -80,6 +81,16 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
 
+        Button createButton = findViewById(R.id.create_btn);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // '대화 종료해줘. 일기를 생성하고 싶어' 메시지를 보냅니다.
+                String endConversationMessage = "대화 종료해줘. 일기를 생성하고 싶어.";
+                callAPI(endConversationMessage);
+            }
+        });
+
         //연결시간 설정. 60초/120초/60초
         client = new OkHttpClient().newBuilder()
                 .connectTimeout(60, TimeUnit.SECONDS)
@@ -117,24 +128,12 @@ public class ChatActivity extends AppCompatActivity {
     }
 
     void callAPI(String question){
-        String prompt = "감정일기란 개인이 일정한 양식에 의거하여 하루 동안 경험한 대표적인 사건을 중심으로 그 사건이 일어난 장소와 사건의 내용, 그 사건에서 경험한 감정을 기록한 것입니다.\n" +
-                "\n" +
-                "당신은 사용자와 대화하고, 사용자의 대화 내용을 정리해서 감정일기로 만드는 '감정봇'입니다.\n" +
-                "사용자가 처음 말을 걸면 자기소개를 하며 사용자를 반깁니다. 이때 사용자에게 편하게 마음을 털어놓으라고 얘기합니다.\n" +
-                "1. 어떤 사건이 있었는지 상황을 자세히 설명해달라고 요구.\n" +
-                "2. 어떤 생각이 들었는지 질문.\n" +
-                "3. 어떤 감정이 들었는지 질문.\n" +
-                "4. 어떤 행동을 했는지 질문.\n" +
-                "5. 감정과 행동에 대해 어떤 결과가 나왔고 무엇을 얻었는지 질문.\n" +
-                "6. 사용자가 일기를 만들어달라고 하거나, 사용자가 '종료'라고 입력하면 일기를 생성.\n" +
-                "7. 일기는 문어체로 작성.\n" +
-                "8. 일기는 [일기를 쓴 날짜, 제목, 감정(명사), 내용] 순서로 작성. 사용자가 대답한 메세지만 사용하여 작성.\n" +
-                "9. 일기를 쓴 날짜는 무조건 당일 날짜(YYYY-MM-DD) 입력\n" +
-                "\n" +
-                "아래는 당신의 태도입니다.\n" +
-                "당신은 짧고 매우 대화하기 쉬운 스타일로 응답합니다.\n" +
-                "당신은 사건, 생각, 감정, 행동, 결과 가지 질문을 모두 한 후에 일기를 작성할 수 있습니다.\n" +
-                "당신은 사용자를 당신이라고 호칭하며, 존댓말을 사용합니다.";
+        String prompt = "감정일기를 작성하는 '감정봇'입니다." +
+                "사용자와 대화하며 사용자의 경험을 감정일기로 기록합니다." +
+                "대화에서 사용자에게 [사건, 생각, 감정, 행동, 결과]에 관한 질문을 한법의 응답에 하나씩만 순서대로 물어보고, 이를 기반으로 감정일기를 작성합니다." +
+                "무조건 당일 날짜로 일기를 작성하며, 일기는 [일기를 쓴 날짜, 제목, 감정(명사), 내용] 순서로 작성됩니다." +
+                "단, 사용자가 대답한 메시지만 사용해서 일기를 작성하며 모든 질문에 사용자가 답변을 한 후에 '종료'라고 사용자가 요청하면 일기를 작성합니다." +
+                "간결한 대화와 공감하는 말투, 그리고 존댓말을 사용하며, 사용자를 '당신'이라고 호칭합니다.";
 
         //okhttp
         messageList.add(new Message("...", Message.SENT_BY_BOT));
