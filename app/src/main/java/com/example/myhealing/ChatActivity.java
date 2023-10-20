b3bfccafd7d5917361e3ddf4f6ca7a6c5248fa5b
Woo_jin <waw1159@gmail.com>
2023-10-20 23:36:41 +0900

    add callDiaryAPI()

--- app/src/main/java/com/example/myhealing/ChatActivity.java
+++ app/src/main/java/com/example/myhealing/ChatActivity.java
@@ -47,13 +47,14 @@ public class ChatActivity extends AppCompatActivity {
     TextView tv_welcome;
     EditText et_msg;
     ImageButton btn_send;
-
+    String diaryResponse = "";
     List<Message> messageList;
     MessageAdapter messageAdapter;
 
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
     OkHttpClient client = new OkHttpClient();
     private List<JSONObject> conversation = new ArrayList<>();
+    List<String> userApiRequests = new ArrayList<>();
 
     private ImageView ivGoBackMenu;
     private ImageView ivCrateDiary;
@@ -104,9 +105,10 @@ public class ChatActivity extends AppCompatActivity {
                 Log.d(TAG, "onClick: 뒤로가기 클릭됨");
                 Toast.makeText(getApplicationContext(), "버튼 클릭", Toast.LENGTH_SHORT).show();
                 // intent 종료
-
+                finish();
                 // 대화 내용 초기화
                 conversation.clear();
+                userApiRequests.clear();
             }
         });
 
@@ -114,10 +116,9 @@ public class ChatActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: 일기 생성 클릭됨");
-                Toast.makeText(getApplicationContext(), "버튼 클릭", Toast.LENGTH_SHORT).show();
-                // '대화 종료해줘. 일기를 생성하고 싶어' 메시지를 보냅니다.
-//                String endConversationMessage = "대화 종료해줘. 일기를 생성하고 싶어.";
-//                callAPI(endConversationMessage);
+                // 일기 생성 메세지 전송
+                callDiaryAPI(userApiRequests.toString());
+                Toast.makeText(getApplicationContext(), "일기 생성", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -159,22 +160,22 @@ public class ChatActivity extends AppCompatActivity {
 
     void callAPI(String question){
         String prompt =
-                "당신은 감정일기를 작성하는 '감정봇'입니다." +
-                "사용자와 대화하며 사용자의 경험을 감정일기로 기록합니다." +
-                "대화에서 사용자에게 [사건, 생각, 감정, 행동, 결과]에 관한 질문을 한번의 응답에 하나씩만 하며, 순서대로 물어보고, 이를 기반으로 감정일기를 작성합니다." +
-                "무조건 대한민국 표준시를 기준으로 당일의 날짜를 작성합니다. 일기는 [날짜:, 제목:, 감정:, 내용:] 순서로 작성합니다. 감정은 명사로 작성합니다." +
-                "일기는 일기체로 작성합니다." +
-                "단, 사용자가 대답한 메시지만 사용해서 일기를 작성합니다. 모든 질문에 사용자가 답변을 한 후에, [사용자가 일기를 만들어달라고 요청하면] 일기를 작성합니다." +
-                "간결하고 공감하는 말투, 그리고 존댓말을 사용하며, 사용자를 '당신'이라고 호칭합니다. '안녕하세요' 라는 인사는 처음 한번만 합니다";
+                "당신은 '감정봇'입니다." +
+                "당신은 사용자와 대화하며 사용자가 감정을 정리할 수 있도록 도와줍니다." +
+                "사용자에게 인사를 하고, 사용자에게 있었던 일을 마음 편히 털어놓으라고 권유합니다." +
+                "그 이후 사용자에게 [사건, 생각, 감정, 행동, 결과]에 관한 질문을 합니다. 질문은 한번의 응답에 하나씩만 하며, 순서대로 질문합니다." +
+                "사용자가 모든 질문에 대답을 하면, 사용자에게 생성 버튼을 눌러 일기를 생성하라고 안내합니다." +
+                "간결하고 쉬운 표현을 사용하고, 사용자에게 공감합니다."+
+                "사용자에게 존댓말을 사용하며, 사용자를 '당신'이라고 호칭합니다.";
+
+        userApiRequests.add(question);
 
         //okhttp
         messageList.add(new Message("...", Message.SENT_BY_BOT));
 
         //추가된 내용
         JSONArray arr = new JSONArray();
-        JSONObject baseAi = new JSONObject();
         JSONObject userMsg = new JSONObject();
-
         // 첫 번째 메시지는 초기 지침을 포함하는 prompt 역할을 합니다.
         JSONObject initialPrompt = new JSONObject();
         try {
@@ -240,11 +241,85 @@ public class ChatActivity extends AppCompatActivity {
         });
     }
 
+    void callDiaryAPI(String userApiRequests) {
+        // 일기 작성 요청을 보낼 prompt
+        String diaryPrompt = "사용자가 쓴 내용으로 일기를 만들어줘. " +
+                "['날짜:'(대한민국 서울시 표준시 기준 오늘 날짜), '제목:', '감정:'(명사로 작성), '내용:'] " +
+                "순서로 작성하고, ''안에 있는 내용('날짝:', '제목:', '감정:', '내용:')은 반드시 넣어줘. " +
+                "일기는 일기체로 작성해줘.";
+
+        //okhttp
+        messageList.add(new Message("...", Message.SENT_BY_BOT));
+
+        //추가된 내용
+        JSONArray arr = new JSONArray();
+        JSONObject baseAi = new JSONObject();
+        JSONObject userMessage = new JSONObject();
+        try {
+            baseAi.put("role", "system");
+            baseAi.put("content", diaryPrompt);
+            //유저 메세지
+            userMessage.put("role", "user");
+            userMessage.put("content", userApiRequests);
+            //array로 담아서 한번에 보낸다
+            arr.put(baseAi);
+            arr.put(userMessage);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        JSONObject object = new JSONObject();
+        try {
+            //모델명 변경
+            object.put("model", "gpt-3.5-turbo");
+            object.put("messages", arr);
+            object.put("max_tokens", 256);
+//            object.put("temperature", 1);
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(object.toString(), JSON);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/chat/completions")
+                .header("Authorization", "Bearer "+MY_SECRET_KEY)
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                addResponse("Failed to load response due to "+e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if(response.isSuccessful()){
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(response.body().string());
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        String result = jsonArray.getJSONObject(0).getJSONObject("message").getString("content");
+                        addResponse(result.trim());
+                        diaryResponse = result.trim();
+                    }catch (JSONException e){
+                        e.printStackTrace();
+                    }
+                } else {
+                    addResponse("Failed to load response due to "+response.body().string());
+                }
+            }
+        });
+
+        System.out.println(diaryResponse);
+
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         // 대화 내용 초기화
         conversation.clear();
+        userApiRequests.clear();
     }
 
 }
