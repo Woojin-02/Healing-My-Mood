88776d2543ac247fef64806bdef4547939e2e6e2
Woo_jin <waw1159@gmail.com>
2023-10-26 22:50:54 +0900

    update list_item.xml and activity_main.xml(motified diarylist)

--- app/src/main/java/com/example/myhealing/adapter/DiaryAdapter.java
+++ app/src/main/java/com/example/myhealing/adapter/DiaryAdapter.java
@@ -11,7 +11,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.myhealing.EmotionalDiary;
 import com.example.myhealing.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 public class DiaryAdapter extends RecyclerView.Adapter<DiaryAdapter.MyViewHolder>{
@@ -29,10 +34,9 @@ public class DiaryAdapter extends RecyclerView.Adapter<DiaryAdapter.MyViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.titleText.setText(diaryList.get(position).title);
-        holder.emotionText.setText(diaryList.get(position).emotion);
+        holder.titleText.setText("제목 : "+diaryList.get(position).title);
+        holder.emotionText.setText("감정 : "+diaryList.get(position).emotion);
         holder.creationText.setText(diaryList.get(position).creationDate);
-        holder.detailText.setText(diaryList.get(position).detail);
     }
 
     @Override
@@ -43,11 +47,28 @@ public class DiaryAdapter extends RecyclerView.Adapter<DiaryAdapter.MyViewHolder
     //리스트 저장
     public void setDiaryList(List<EmotionalDiary> diaryList){
         if (diaryList != null) {
+            // creationDate에 따라 리스트를 내림차순으로 정렬
+            Collections.sort(diaryList, new Comparator<EmotionalDiary>() {
+                @Override
+                public int compare(EmotionalDiary diary1, EmotionalDiary diary2) {
+                    // creationDate를 파싱하여 비교
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                    try {
+                        Date date1 = sdf.parse(diary1.creationDate);
+                        Date date2 = sdf.parse(diary2.creationDate);
+                        return date2.compareTo(date1); // creationDate가 최신 순으로 정렬
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    return 0;
+                }
+            });
+
             this.diaryList = diaryList;
         } else {
-            this.diaryList = new ArrayList<>(); // 빈 목록 생성 또는 처리 방법에 따라 다름
+            this.diaryList = new ArrayList<>();  // 빈 목록 생성
         }
-        notifyDataSetChanged(); // 어댑터에 변경 사항 알리기
+        notifyDataSetChanged();  // 어댑터에 변경 사항 알리기
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder{
@@ -60,7 +81,6 @@ public class DiaryAdapter extends RecyclerView.Adapter<DiaryAdapter.MyViewHolder
             titleText = itemView.findViewById(R.id.title_text);
             emotionText = itemView.findViewById(R.id.emotion_text);
             creationText = itemView.findViewById(R.id.creation_text);
-            detailText = itemView.findViewById(R.id.detail_text);
         }
     }
 }
