44197d7688bba109d2cc1863c55a83fb964a6221
Woo_jin <waw1159@gmail.com>
2023-11-12 14:58:34 +0900

    create HomeCalendarActivity

---
+++ app/src/main/java/com/example/myhealing/HomeCalendarActivity.java
@@ -0,0 +1,13 @@
+package com.example.myhealing;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeCalendarActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homediary);
+    }
+}
